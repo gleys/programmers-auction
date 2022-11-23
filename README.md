c47eb07623a582e396c37830fa6f8301b2d94c4a
gleys <hse6042@naver.com>
2022-11-23 12:32:17 +0900

    feat: 백엔드 프로젝트 생성

---
+++ README.md
@@ -0,0 +1,2 @@
+# programmer-auction
+# programmers-auction
---
+++ src/test/java/com/example/programmerauction/ProgrammerAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.programmerauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammerAuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
